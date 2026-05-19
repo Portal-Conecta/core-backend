f26de05932389f94222954684fcced62bc7bb139
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-05-19 18:29:39 -0300

    #29 feat: implementa política de validação de e-mail para criação de usuário

---
+++ src/main/java/com/portal/conecta/hub/module/user/domain/exception/UserPermissionDeniedException.java
@@ -0,0 +1,8 @@
+package com.portal.conecta.hub.module.user.domain.exception;
+
+public class UserPermissionDeniedException extends UserException {
+
+    public UserPermissionDeniedException(String message) {
+        super(message);
+    }
+}
