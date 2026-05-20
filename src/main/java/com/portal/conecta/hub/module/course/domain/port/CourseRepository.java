9a2f2137ef8bca4f0019a6c8b36d76cde879a5ba
Kaua Felix <kfelixcosta11@gmail.com>
2026-05-19 21:52:35 -0300

    #28 feat: adiciona repositórios para as entidades de turma e curso

---
+++ src/main/java/com/portal/conecta/hub/module/course/domain/port/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.portal.conecta.hub.module.course.domain.port;
+
+import com.portal.conecta.hub.module.course.domain.model.CourseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CourseRepository extends JpaRepository<CourseEntity, UUID> {
+}
