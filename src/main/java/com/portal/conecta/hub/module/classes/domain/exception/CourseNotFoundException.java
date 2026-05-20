9ea23b645099eee93d9add7feb184c79852b7895
Kaua Felix <kfelixcosta11@gmail.com>
2026-05-19 21:50:34 -0300

    #28 feat: adiciona exceptions customizadas para dados invalidos da turma e que o curso nao existe

---
+++ src/main/java/com/portal/conecta/hub/module/classes/domain/exception/CourseNotFoundException.java
@@ -0,0 +1,7 @@
+package com.portal.conecta.hub.module.classes.domain.exception;
+
+public class CourseNotFoundException extends RuntimeException {
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+}
