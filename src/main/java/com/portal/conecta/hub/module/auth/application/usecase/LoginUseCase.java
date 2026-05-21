aad8c8f30044bed16e512acdc8f87529815bb81f
leticiaguths <leticiaguths83@gmail.com>
2026-05-20 21:25:11 -0300

    refactor: busca classMemberships separadamente no LoginUseCase para evitar LazyInitializationException

--- src/main/java/com/portal/conecta/hub/module/auth/application/usecase/LoginUseCase.java
+++ src/main/java/com/portal/conecta/hub/module/auth/application/usecase/LoginUseCase.java
@@ -6,31 +6,38 @@ import com.portal.conecta.hub.module.auth.domain.model.AuthUser;
 import com.portal.conecta.hub.module.auth.domain.port.TokenProviderPort;
 import com.portal.conecta.hub.module.auth.infrastructure.security.JwtProperties;
 import com.portal.conecta.hub.module.auth.presentation.dto.LoginResponse;
+import com.portal.conecta.hub.module.classes.domain.model.ClassMembershipEntity;
+import com.portal.conecta.hub.module.classes.domain.port.ClassMembershipRepository;
 import com.portal.conecta.hub.module.user.domain.port.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class LoginUseCase {
 
     private final TokenProviderPort tokenProviderPort;
-    private final JwtProperties jwtProperties;
     private final PasswordEncoder passwordEncoder;
+
     private final UserRepository repository;
+    private final ClassMembershipRepository membershipRepository;
 
     public LoginResponse execute(LoginCommand command) {
 
-        AuthUser user =  repository.findByEmail(command.email())
+        AuthUser user = repository.findByEmail(command.email())
                 .orElseThrow(() -> new AuthException("Email ou senha inválidos"));
 
-        if(!passwordEncoder.matches(command.password(), user.getPasswordHash())) {
+        List<ClassMembershipEntity> membershipEntities = membershipRepository.findAllByUserId(user.getId());
+
+        if (!passwordEncoder.matches(command.password(), user.getPasswordHash())) {
             throw new RuntimeException("Email ou senha inválidos");
         }
 
-        String accessToken = tokenProviderPort.generateAccessToken(user);
+        String accessToken = tokenProviderPort.generateAccessToken(user, membershipEntities);
         String refreshToken = tokenProviderPort.generateRefreshToken(user);
 
         return new LoginResponse(accessToken, refreshToken, 900L);
