3c31500e813ef06951b60d818c10fc4b8f92bdde
Kauã Felix <kfelixcosta11@gmail.com>
2026-05-20 21:21:12 -0300

    #28 feat: adiciona método para encontrar o último número de turma por ID de curso com bloqueio pessimista

--- src/main/java/com/portal/conecta/hub/module/classes/domain/port/ClassRepository.java
+++ src/main/java/com/portal/conecta/hub/module/classes/domain/port/ClassRepository.java
@@ -1,9 +1,19 @@
 package com.portal.conecta.hub.module.classes.domain.port;
 
 import com.portal.conecta.hub.module.classes.domain.model.ClassEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClassRepository extends JpaRepository<ClassEntity, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT MAX(c.number) FROM ClassEntity c WHERE c.course.id = :courseId")
+    Optional<Integer> findLastNumberByCourseId(@Param("courseId") UUID courseId);
+
 }
