ef2d148a9471036ae28cee7f5ab68fff05417ed7
Kauã Felix <kfelixcosta11@gmail.com>
2026-05-25 21:05:54 -0300

    feat: remove mensagens de validação desnecessárias do UpdateUserRequest.

--- src/main/java/com/portal/conecta/hub/module/user/presentation/dto/request/UpdateUserRequest.java
+++ src/main/java/com/portal/conecta/hub/module/user/presentation/dto/request/UpdateUserRequest.java
@@ -4,11 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdateUserRequest(
-
-        @NotBlank(message = "name is required.")
         String name,
-        @NotBlank(message = "email is required.")
-        @Email(message = "email must be valid.")
+        @Email(message = "email must be valid")
         String email,
         String avatarUrl
 ) {
