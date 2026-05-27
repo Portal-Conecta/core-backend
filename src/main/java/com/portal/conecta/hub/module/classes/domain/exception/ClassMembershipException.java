41987e96d4773ccd2143e1ada362d763d0408deb
OrtizGuilhermex <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-05-27 16:47:15 -0300

    #52 feat: adiciona ClassMembershipException e ClassNotFoundException

---
+++ src/main/java/com/portal/conecta/hub/module/classes/domain/exception/ClassMembershipException.java
@@ -0,0 +1,7 @@
+package com.portal.conecta.hub.module.classes.domain.exception;
+
+public class ClassMembershipException extends RuntimeException {
+    public ClassMembershipException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/portal/conecta/hub/module/classes/domain/exception/ClassNotFoundException.java
@@ -0,0 +1,7 @@
+package com.portal.conecta.hub.module.classes.domain.exception;
+
+public class ClassNotFoundException extends RuntimeException {
+    public ClassNotFoundException(String message) {
+        super(message);
+    }
+}
