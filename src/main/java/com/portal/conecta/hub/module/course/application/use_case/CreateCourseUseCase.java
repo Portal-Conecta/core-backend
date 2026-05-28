94cda392f797fb23a949002b18a264e0535a9932
leticiaguths <leticiaguths83@gmail.com>
2026-05-28 18:21:33 -0300

    feat: implementa caso de uso para criação de curso

--- src/main/java/com/portal/conecta/hub/module/course/application/use_case/CreateCourseUseCase.java
+++ src/main/java/com/portal/conecta/hub/module/course/application/use_case/CreateCourseUseCase.java
@@ -1,4 +1,24 @@
 package com.portal.conecta.hub.module.course.application.use_case;
 
+import com.portal.conecta.hub.module.course.application.command.CreateCourseCommand;
+import com.portal.conecta.hub.module.course.domain.model.CourseEntity;
+import com.portal.conecta.hub.module.course.domain.port.CourseRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
 public class CreateCourseUseCase {
+
+    private final CourseRepository courseRepository;
+
+    public CreateCourseUseCase(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @Transactional
+    public CourseEntity execute(CreateCourseCommand courseCommand) {
+        CourseEntity course = CourseEntity.create(courseCommand.name(), courseCommand.code());
+        return courseRepository.save(course);
+    }
+
 }
