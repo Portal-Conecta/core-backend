182475279763061a9474be0d852edb7e446fdbde
leticiaguths <leticiaguths83@gmail.com>
2026-05-28 18:21:56 -0300

    feat: adiciona DTOs para criação de curso

--- src/main/java/com/portal/conecta/hub/module/course/presentation/dto/CreateCourseResponse.java
+++ src/main/java/com/portal/conecta/hub/module/course/presentation/dto/CreateCourseResponse.java
@@ -1,4 +1,24 @@
 package com.portal.conecta.hub.module.course.presentation.dto;
 
-public record CreateCourseResponse() {
+import com.portal.conecta.hub.module.course.domain.model.CourseEntity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record CreateCourseResponse(
+        UUID id,
+        String name,
+        String code,
+        Instant createdAt
+) {
+
+    public static CreateCourseResponse from(CourseEntity courseEntity) {
+        return new CreateCourseResponse(
+                courseEntity.getId(),
+                courseEntity.getName(),
+                courseEntity.getCode(),
+                courseEntity.getCreatedAt()
+        );
+    }
+
 }
