867f49a95e4eea9796b39677ebc6d2626a788703
Kauã Felix <kfelixcosta11@gmail.com>
2026-05-28 18:51:58 -0300

    #34 refactor: unifica validação de permissões para remoção de turma

--- src/main/java/com/portal/conecta/hub/module/classes/domain/validator/ClassPermissionValidator.java
+++ src/main/java/com/portal/conecta/hub/module/classes/domain/validator/ClassPermissionValidator.java
@@ -2,7 +2,6 @@ package com.portal.conecta.hub.module.classes.domain.validator;
 
 import com.portal.conecta.hub.module.user.domain.exception.UserPermissionDeniedException;
 import com.portal.conecta.hub.module.user.domain.model.TypeUser;
-import com.portal.conecta.hub.shared.exception.UnauthorizedUserException;
 import org.springframework.stereotype.Component;
 
 import java.util.EnumSet;
@@ -21,15 +20,9 @@ public class ClassPermissionValidator {
         return PERMISSION_TYPES.contains(type);
     }
 
-    private static final EnumSet<TypeUser> DELETE_PERMISSION_TYPES = EnumSet.of(
-            TypeUser.ADMIN,
-            TypeUser.WEG,
-            TypeUser.SENAI
-    );
-
     public boolean canDelete(TypeUser type){
         if (type == null) return false;
-        return DELETE_PERMISSION_TYPES.contains(type);
+        return PERMISSION_TYPES.contains(type);
     }
 
     public void validateCanDelete (TypeUser type){
