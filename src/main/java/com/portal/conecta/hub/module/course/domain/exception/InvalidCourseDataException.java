333f8759ca77d031b3e2d8c1a47f972872bb82d7
leticiaguths <leticiaguths83@gmail.com>
2026-05-28 19:05:40 -0300

    feat: adiciona exceptions de validação para criação de curso

---
+++ src/main/java/com/portal/conecta/hub/module/course/domain/exception/InvalidCourseDataException.java
@@ -0,0 +1,7 @@
+package com.portal.conecta.hub.module.course.domain.exception;
+
+public class InvalidCourseDataException extends RuntimeException {
+    public InvalidCourseDataException(String message) {
+        super(message);
+    }
+}
