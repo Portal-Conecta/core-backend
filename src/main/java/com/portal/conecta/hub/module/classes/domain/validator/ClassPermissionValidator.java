511a806f84bbc4db4659999fa5435bd6f4651d17
Kauã Felix <kfelixcosta11@gmail.com>
2026-05-28 16:44:36 -0300

    #34 feat: implementa funcionalidade para deletar turma com validação de permissões e tratamento de exceções

--- src/main/java/com/portal/conecta/hub/module/classes/domain/validator/ClassPermissionValidator.java
+++ src/main/java/com/portal/conecta/hub/module/classes/domain/validator/ClassPermissionValidator.java
@@ -1,6 +1,8 @@
 package com.portal.conecta.hub.module.classes.domain.validator;
 
+import com.portal.conecta.hub.module.user.domain.exception.UserPermissionDeniedException;
 import com.portal.conecta.hub.module.user.domain.model.TypeUser;
+import com.portal.conecta.hub.shared.exception.UnauthorizedUserException;
 import org.springframework.stereotype.Component;
 
 import java.util.EnumSet;
@@ -17,5 +19,22 @@ public class ClassPermissionValidator {
     public boolean canCreate(TypeUser type) {
         if (type == null) return false;
         return PERMISSION_TYPES.contains(type);
+    }
+
+    private static final EnumSet<TypeUser> DELETE_PERMISSION_TYPES = EnumSet.of(
+            TypeUser.ADMIN,
+            TypeUser.WEG,
+            TypeUser.SENAI
+    );
+
+    public boolean canDelete(TypeUser type){
+        if (type == null) return false;
+        return DELETE_PERMISSION_TYPES.contains(type);
+    }
+
+    public void validateCanDelete (TypeUser type){
+        if (!canDelete(type)){
+            throw new UserPermissionDeniedException("User does not have permission to delete a class.");
+        }
     }
 }
