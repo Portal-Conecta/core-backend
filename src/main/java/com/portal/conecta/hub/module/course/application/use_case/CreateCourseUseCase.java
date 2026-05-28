eff0f07959544efd228754394e1fc47fb84792dd
leticiaguths <leticiaguths83@gmail.com>
2026-05-28 19:06:29 -0300

    feat: completa implementação do caso de uso de criação de curso

--- src/main/java/com/portal/conecta/hub/module/course/application/use_case/CreateCourseUseCase.java
+++ src/main/java/com/portal/conecta/hub/module/course/application/use_case/CreateCourseUseCase.java
@@ -1,8 +1,18 @@
 package com.portal.conecta.hub.module.course.application.use_case;
 
 import com.portal.conecta.hub.module.course.application.command.CreateCourseCommand;
+import com.portal.conecta.hub.module.course.domain.exception.CourseCodeAlreadyInUseException;
+import com.portal.conecta.hub.module.course.domain.exception.CourseNameAlreadyInUseException;
+import com.portal.conecta.hub.module.course.domain.exception.InvalidCourseDataException;
 import com.portal.conecta.hub.module.course.domain.model.CourseEntity;
 import com.portal.conecta.hub.module.course.domain.port.CourseRepository;
+import com.portal.conecta.hub.module.course.domain.validator.CoursePermissionValidator;
+import com.portal.conecta.hub.module.user.domain.exception.UserNotFoundException;
+import com.portal.conecta.hub.module.user.domain.model.UserEntity;
+import com.portal.conecta.hub.module.user.domain.port.UserRepository;
+import com.portal.conecta.hub.shared.context.RequestContext;
+import com.portal.conecta.hub.shared.context.RequestContextProvider;
+import com.portal.conecta.hub.shared.exception.UnauthorizedUserException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,15 +20,52 @@ import org.springframework.transaction.annotation.Transactional;
 public class CreateCourseUseCase {
 
     private final CourseRepository courseRepository;
+    private final CoursePermissionValidator permissionValidator;
+    private final RequestContextProvider requestProvider;
+    private final UserRepository userRepository;
 
-    public CreateCourseUseCase(CourseRepository courseRepository) {
+    public CreateCourseUseCase(CourseRepository courseRepository,
+                               CoursePermissionValidator permissionValidator,
+                               RequestContextProvider requestProvider,
+                               UserRepository userRepository) {
         this.courseRepository = courseRepository;
+        this.permissionValidator = permissionValidator;
+        this.requestProvider = requestProvider;
+        this.userRepository = userRepository;
     }
 
     @Transactional
     public CourseEntity execute(CreateCourseCommand courseCommand) {
+        CreateCourseCommand validCommand = requireCommand(courseCommand);
+
+        RequestContext context = requestProvider.getRequestContext();
+
+        if (!permissionValidator.canCreate(context.userType())) {
+            throw new UnauthorizedUserException();
+        }
+
+        UserEntity createdBy = userRepository.findById(context.userId())
+                .orElseThrow(() -> new UserNotFoundException("User not found: " + context.userId()));
+
+        if (courseRepository.existsByName(courseCommand.name())) {
+            throw new CourseNameAlreadyInUseException("Name already in use: " + courseCommand.name());
+        }
+
+        if (courseRepository.existsByCode(courseCommand.code())) {
+            throw new CourseCodeAlreadyInUseException("Code already in use: " + courseCommand.code());
+        }
+
         CourseEntity course = CourseEntity.create(courseCommand.name(), courseCommand.code());
+        course.setCreatedBy(createdBy);
+
         return courseRepository.save(course);
+    }
+
+    private CreateCourseCommand requireCommand(CreateCourseCommand command){
+        if (command == null){
+            throw new InvalidCourseDataException("");
+        }
+        return command;
     }
 
 }
