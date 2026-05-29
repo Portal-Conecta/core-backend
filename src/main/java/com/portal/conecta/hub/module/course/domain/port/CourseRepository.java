0db31361fa0ee8d250d3324d63fda623cd92399a
Kauã Felix <kfelixcosta11@gmail.com>
2026-05-28 21:37:57 -0300

    #56 feat: reorganiza exceção CourseNotFoundException e atualiza repositório de cursos

--- src/main/java/com/portal/conecta/hub/module/course/domain/port/CourseRepository.java
+++ src/main/java/com/portal/conecta/hub/module/course/domain/port/CourseRepository.java
@@ -3,6 +3,8 @@ package com.portal.conecta.hub.module.course.domain.port;
 import com.portal.conecta.hub.module.course.domain.model.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CourseRepository extends JpaRepository<CourseEntity, UUID> {
@@ -10,4 +12,8 @@ public interface CourseRepository extends JpaRepository<CourseEntity, UUID> {
     boolean existsByName(String name);
 
     boolean existsByCode(String code);
+
+    Optional<CourseEntity> findByIdAndDeletedAtIsNull(UUID id);
+
+    List<CourseEntity> findAllByDeletedAtIsNull();
 }
