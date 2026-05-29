9d8a22337ad2f493596bfcc21582da8d7b57a693
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-05-29 19:08:03 -0300

    #66 test: adiciona ClassMembershipNotFoundException para retorno 404

---
+++ src/main/java/com/portal/conecta/hub/module/classes/domain/exception/ClassMembershipNotFoundException.java
@@ -0,0 +1,7 @@
+package com.portal.conecta.hub.module.classes.domain.exception;
+
+public class ClassMembershipNotFoundException extends RuntimeException {
+    public ClassMembershipNotFoundException(String message) {
+        super(message);
+    }
+}
