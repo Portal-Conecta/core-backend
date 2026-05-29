02a3c03aa230b63d5be35738dd9fcf8cb2792f26
Kauã Felix <kfelixcosta11@gmail.com>
2026-05-28 21:37:57 -0300

    #56 feat: reorganiza exceção CourseNotFoundException e atualiza repositório de cursos

--- src/main/java/com/portal/conecta/hub/module/course/domain/exception/CourseNotFoundException.java
+++ src/main/java/com/portal/conecta/hub/module/course/domain/exception/CourseNotFoundException.java
@@ -1,4 +1,4 @@
-package com.portal.conecta.hub.module.classes.domain.exception;
+package com.portal.conecta.hub.module.course.domain.exception;
 
 public class CourseNotFoundException extends RuntimeException {
     public CourseNotFoundException(String message) {
