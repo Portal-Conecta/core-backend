6e31ab7c4f99d51ff50754472dd5f8feee99a791
Kauã Felix <kfelixcosta11@gmail.com>
2026-05-29 16:45:37 -0300

    #53 feat: implementa caso de uso e controlador para obter cursos do usuário

---
+++ src/main/java/com/portal/conecta/hub/module/me/application/use_case/GetMyCoursesUseCase.java
@@ -0,0 +1,67 @@
+package com.portal.conecta.hub.module.me.application.use_case;
+
+import com.portal.conecta.hub.module.classes.domain.port.ClassMembershipRepository;
+import com.portal.conecta.hub.module.me.infraestrutura.projection.UserCouseClassProjection;
+import com.portal.conecta.hub.module.me.presentation.dto.MyClassResponse;
+import com.portal.conecta.hub.module.me.presentation.dto.MyCourseResponse;
+import com.portal.conecta.hub.module.me.presentation.dto.MyListCourseResponse;
+import com.portal.conecta.hub.shared.context.RequestContextProvider;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class GetMyCoursesUseCase {
+
+    private final ClassMembershipRepository classMembershipRepository;
+    private final RequestContextProvider requestContext;
+
+    public GetMyCoursesUseCase(ClassMembershipRepository classMembershipRepository, RequestContextProvider requestContext) {
+        this.classMembershipRepository = classMembershipRepository;
+        this.requestContext = requestContext;
+    }
+
+    public MyListCourseResponse execute(){
+        UUID userId = requestContext
+                .getRequestContext()
+                .userId();
+
+        List<UserCouseClassProjection> rows =
+                classMembershipRepository.findCoursesByUserId(userId);
+
+        LinkedHashMap<UUID, MyCourseResponse> coursesMap =
+                new LinkedHashMap<>();
+
+        for (UserCouseClassProjection row : rows){
+            MyCourseResponse course =
+                    coursesMap.computeIfAbsent(
+                            row.getCourseId(),
+                            id -> new MyCourseResponse(
+                                    row.getCourseId(),
+                                    row.getCourseName(),
+                                    row.getCourseCode(),
+                                    new ArrayList<>()
+                            )
+                    );
+
+            course.classes().add(
+                    new MyClassResponse(
+                            row.getClassId(),
+                            row.getClassName(),
+                            row.getClassNumber(),
+                            row.getClassShift(),
+                            row.getRole()
+                    )
+            );
+        }
+
+        return new MyListCourseResponse(
+                new ArrayList<>(coursesMap.values())
+        );
+    }
+
+
+}
