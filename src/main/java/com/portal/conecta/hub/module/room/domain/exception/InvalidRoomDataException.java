a24898104b7e2bedeb8e88f16566930837f3a3c0
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-05-28 21:47:53 -0300

    #61 feat: implementa exceções de domínio para criação de sala

---
+++ src/main/java/com/portal/conecta/hub/module/room/domain/exception/InvalidRoomDataException.java
@@ -0,0 +1,7 @@
+package com.portal.conecta.hub.module.room.domain.exception;
+
+public class InvalidRoomDataException extends RuntimeException{
+    public InvalidRoomDataException(String message) {
+        super(message);
+    }
+}
