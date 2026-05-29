a5d6b86a915cbf5056a2d99ffab72560785229e3
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-05-29 20:54:40 -0300

    #73 docs: implementa documentacao com swagger nos request

--- src/main/java/com/portal/conecta/hub/module/classes/presentation/dto/AddMemberRequest.java
+++ src/main/java/com/portal/conecta/hub/module/classes/presentation/dto/AddMemberRequest.java
@@ -1,12 +1,26 @@
 package com.portal.conecta.hub.module.classes.presentation.dto;
 
 import com.portal.conecta.hub.module.classes.domain.model.ClassRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
+@Schema(description = "Payload para vincular um usuário a uma turma.")
 public record AddMemberRequest(
-        @NotNull UUID userId,
-        @NotNull ClassRole classRole
+
+        @Schema(
+                description = "Identificador único do usuário que será adicionado à turma.",
+                example = "987e6543-e21b-34d5-c678-426614174999"
+        )
+        @NotNull
+        UUID userId,
+
+        @Schema(
+                description = "Papel que o usuário exercerá na turma (ex: STUDENT, TEACHER).",
+                example = "STUDENT"
+        )
+        @NotNull
+        ClassRole classRole
 ) {
 }
