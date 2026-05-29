45ad9042c6a50a4e7d7e911684fccd59fe0cb2f4
Kauã Felix <kfelixcosta11@gmail.com>
2026-05-29 20:24:58 -0300

    #65 feat: implementa casos de uso para consultar todas as salas e buscar sala por ID

--- src/main/java/com/portal/conecta/hub/module/room/presentation/controller/RoomController.java
+++ src/main/java/com/portal/conecta/hub/module/room/presentation/controller/RoomController.java
@@ -1,27 +1,33 @@
 package com.portal.conecta.hub.module.room.presentation.controller;
 
 import com.portal.conecta.hub.module.room.application.use_case.CreateRoomUseCase;
+import com.portal.conecta.hub.module.room.application.use_case.GetAllRoomUseCase;
+import com.portal.conecta.hub.module.room.application.use_case.GetRoomByIdUseCase;
 import com.portal.conecta.hub.module.room.presentation.dto.CreateRoomRequest;
 import com.portal.conecta.hub.module.room.presentation.dto.CreateRoomResponse;
+import com.portal.conecta.hub.module.room.presentation.dto.RoomResponse;
 import com.portal.conecta.hub.module.room.presentation.mapper.RoomMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/rooms")
 public class RoomController {
 
     private final CreateRoomUseCase createRoomUseCase;
+    private final GetAllRoomUseCase getAllRoomUseCase;
+    private final GetRoomByIdUseCase getRoomByIdUseCase;
     private final RoomMapper roomMapper;
 
-    public RoomController(CreateRoomUseCase createRoomUseCase, RoomMapper roomMapper) {
+    public RoomController(CreateRoomUseCase createRoomUseCase, GetAllRoomUseCase getAllRoomUseCase, GetRoomByIdUseCase getRoomByIdUseCase, RoomMapper roomMapper) {
         this.createRoomUseCase = createRoomUseCase;
+        this.getAllRoomUseCase = getAllRoomUseCase;
+        this.getRoomByIdUseCase = getRoomByIdUseCase;
         this.roomMapper = roomMapper;
     }
 
@@ -31,4 +37,22 @@ public class RoomController {
         return ResponseEntity.created(URI.create("/rooms/" + room.getId()))
                 .body(CreateRoomResponse.from(room));
     }
+
+    @GetMapping
+    public ResponseEntity<List<RoomResponse>> findAll(){
+        List<RoomResponse> rooms = getAllRoomUseCase.execute()
+                .stream()
+                .map(RoomResponse::from)
+                .toList();
+        return ResponseEntity.ok(rooms);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoomResponse> findById(@PathVariable UUID id){
+        return ResponseEntity.ok(
+                RoomResponse.from(getRoomByIdUseCase.execute(id))
+        );
+    }
+
+
 }
