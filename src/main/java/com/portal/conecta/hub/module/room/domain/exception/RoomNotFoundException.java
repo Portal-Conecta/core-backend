7876fc16fc5a7c04f3cabc946abf3a72e2f3cb88
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-06-02 20:07:49 -0300

    #61 fix: corrige formatação do RoomNotFoundException

--- src/main/java/com/portal/conecta/hub/module/room/domain/exception/RoomNotFoundException.java
+++ src/main/java/com/portal/conecta/hub/module/room/domain/exception/RoomNotFoundException.java
@@ -1,6 +1,7 @@
 package com.portal.conecta.hub.module.room.domain.exception;
 
 public class RoomNotFoundException extends RuntimeException {
-    public RoomNotFoundException(String message) { super(message);
+    public RoomNotFoundException(String message) {
+        super(message);
     }
 }
