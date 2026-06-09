6f476c6d6b945ed03354478f9a020209510d56bd
OrtizGuilhermex <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-06-09 18:23:38 -0300

    #93 feat: implementa metodos de busca de usuarios ativos no repository

--- src/main/java/com/portal/conecta/hub/module/user/domain/port/UserRepository.java
+++ src/main/java/com/portal/conecta/hub/module/user/domain/port/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,4 +25,9 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Page<UserEntity> findByDeletedAtIsNull(Pageable pageable);
 
     Page<UserEntity> findByDeletedAtIsNullAndType(TypeUser type, Pageable pageable);
+
+    Optional<UserEntity> findByIdAndDeletedAtIsNullAndActiveTrue(UUID id);
+
+    List<UserEntity> findAllByIdInAndDeletedAtIsNullAndActiveTrue(List<UUID> ids);
+
 }
