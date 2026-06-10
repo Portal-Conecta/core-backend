0d7408320c039c8ebe175792429620259b6b4ece
Felix <kfelixcosta11@gmail.com>
2026-06-10 16:03:32 -0300

    #96 feat: implementa endpoint para listar turmas com paginação

---
+++ src/main/java/com/portal/conecta/hub/module/classes/application/use_case/GetAllClassesUseCase.java
@@ -0,0 +1,30 @@
+package com.portal.conecta.hub.module.classes.application.use_case;
+
+import com.portal.conecta.hub.module.classes.application.query.ListClassesQuery;
+import com.portal.conecta.hub.module.classes.domain.model.ClassEntity;
+import com.portal.conecta.hub.module.classes.domain.port.ClassRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Component
+public class GetAllClassesUseCase {
+
+    private final ClassRepository classRepository;
+
+    public GetAllClassesUseCase(ClassRepository classRepository) {
+        this.classRepository = classRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClassEntity> execute (ListClassesQuery query){
+        Objects.requireNonNull(query, "query is required");
+
+        boolean onlyInactive = query.onlyInactive();
+        boolean includeInactive = query.includeInactive();
+
+        return classRepository.findAllWithFilter(includeInactive, onlyInactive, query.toPageRequest());
+    }
+}
