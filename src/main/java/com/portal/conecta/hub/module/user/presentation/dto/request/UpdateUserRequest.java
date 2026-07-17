04d1f748bf37d12069897746c48747f9579e39c8
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-07-17 18:55:58 -0300

    #279 refactor: limita atualizacao ao nome

--- src/main/java/com/portal/conecta/hub/module/user/presentation/dto/request/UpdateUserRequest.java
+++ src/main/java/com/portal/conecta/hub/module/user/presentation/dto/request/UpdateUserRequest.java
@@ -1,12 +1,6 @@
 package com.portal.conecta.hub.module.user.presentation.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
 public record UpdateUserRequest(
-        String name,
-        @Email(message = "e-mail deve ser válido")
-        String email,
-        String avatarUrl
+        String name
 ) {
 }
